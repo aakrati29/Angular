0e0079587ed43852949468acb20dad5779741c8c
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-03-06 00:41:05 +0530

    [Aakrati] add. Display BridgeLabz Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'First Angular Project';
+  logoUrl: string = 'images.jpeg';
   message: string = 'Hello from BridgeLabz';
+  
 }
