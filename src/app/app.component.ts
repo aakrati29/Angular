a9028e3c45a84a835663d0dbd42636b95c31aa9c
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-03-06 10:16:53 +0530

    [Aakrati] add. Validate input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,20 @@ import { FormsModule } from '@angular/forms';
 export class AppComponent {
   title = 'First Angular Project';
   logoUrl: string = 'images.jpeg';
-  message: string = 'Hello from BridgeLabz';
+  message: string = 'Hello from BridgeLabz using One Way Data Bindings';
   bridgeLabzUrl:string="https://www.bridgelabz.com/";
   userName: string = '';
+  errorMessage:string='';
 
   openBridgeLabz(): void {
     window.open(this.bridgeLabzUrl, '_blank');
+  }
+  validateInput(){
+    const regex = RegExp('^[A-Z]{1}[a-z//s]{2,}$');
+    if(!regex.test(this.userName)){
+      this.errorMessage="Invalid name";
+      return;
+    }
+    this.errorMessage='';
   }
 }
